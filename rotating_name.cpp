227ba9239f750c982c4a249efffdd9e0fe28c3c4
Ty <94950455+Ptygh@users.noreply.github.com>
2025-06-25 16:32:08 -0700

    Create rotating_name.cpp
    Outlined 2D equations for block letters

---
+++ rotating_name.cpp
@@ -0,0 +1,9 @@
+/* 
+T equation:
+  -4.5 <= x <= -0.5 {1 <= y <= 2} top bar
+  -3 <= x <= -2 {-2 <= y <= 1} stem
+Y equation:
+  -y + 2 <= x <= -y + 3 {0 <= y <= 2} left arm
+  y + 2 <= x <= y + 3 {0 <= y <= 2} right arm
+  2 <= x <= 3 {-2 <= y <= 0} stem
+*/
